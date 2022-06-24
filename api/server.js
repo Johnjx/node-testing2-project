839e65f7f0ac171a5307ed305b8d3b649e35333e
John Ruddock <john.jxr@pm.me>
2022-06-24 10:55:47 -0400

    server up

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "404 Not found" });
+});
+
+server.use((err, req, res, next) => {// eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
