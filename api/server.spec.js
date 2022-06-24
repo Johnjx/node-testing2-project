71b662a1ba9ace1f8c54e66e1623feaa83d15307
John Ruddock <john.jxr@pm.me>
2022-06-24 17:40:34 -0400

    pre-test DB setup in place, findAll model tested

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,7 +1,35 @@
-test('sanity 1+1', () => {
-   expect(1+1).toEqual(2);
+const db = require("../data/db-config");
+const classes = require('./wow-classes/model');
+
+test("sanity 1+1", () => {
+  expect(1 + 1).toEqual(2);
 });
 
-test('Check environment var', () => {
-    console.log(process.env.NODE_ENV);
+test("Check environment var", () => {
+  console.log(process.env.NODE_ENV);
+  expect(process.env.NODE_ENV).toBe("testing");
 });
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db("resources").truncate();
+  await db("classes").truncate();
+  await db.seed.run();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+describe('DB model tests', () => {
+
+    test('findAll', async () => {
+        const result = await classes.findAll();
+        expect(result).toHaveLength(2);
+        expect(result[1]).toHaveProperty('name', 'shaman');
+    })
+})
