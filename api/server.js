f4e68598df0439c47c607f7ed9369b7f8cd141ac
John Ruddock <john.jxr@pm.me>
2022-06-24 18:21:48 -0400

    moved endpoints to server file, GET classes test set

--- api/server.js
+++ api/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const server = express();
-const classesRoute = require('./wow-classes/router');
+const classes = require('./wow-classes/model');
 
 server.use(express.json());
-server.use('/api/classes', classesRoute);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get('/classes', (req, res, next) => {
+  classes.findAll()
+  .then(result => res.json(result))
+  .catch(next)
+});
+
+server.post('/classes', (req, res) => {
+  res.send('Hello from wow-classes post route');
+});
+
+server.get('/classes/:id', (req, res) => {
+  res.send('Hello from wow-classes get by ID route');
+});
 
 server.use("*", (req, res) => {
   res.status(404).json({ message: "404 Not found" });
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,20 +1,21 @@
 const db = require("../data/db-config");
-const classes = require('./wow-classes/model');
-
+const classes = require("./wow-classes/model");
+const server = require('./server');
+const request = require('supertest');
 
 beforeAll(async () => {
-    await db.migrate.rollback();
-    await db.migrate.latest();
+  await db.migrate.rollback();
+  await db.migrate.latest();
 });
 
 beforeEach(async () => {
-    await db("resources").truncate();
-    await db("classes").truncate();
-    await db.seed.run();
+  await db("resources").truncate();
+  await db("classes").truncate();
+  await db.seed.run();
 });
 
 afterAll(async () => {
-    await db.destroy();
+  await db.destroy();
 });
 
 test("sanity 1+1", () => {
@@ -22,36 +23,54 @@ test("sanity 1+1", () => {
 });
 
 test("Check environment var", () => {
-    expect(process.env.NODE_ENV).toBe("testing");
+  expect(process.env.NODE_ENV).toBe("testing");
 });
 
-describe('DB model tests', () => {
+describe("DB model tests", () => {
 
-    test('findAll()', async () => {
-        const result = await classes.findAll();
-        expect(result).toHaveLength(2);
-        expect(result[1]).toHaveProperty('name', 'shaman');
-    })
+  test("findAll()", async () => {
+    const result = await classes.findAll();
+    expect(result).toHaveLength(2);
+    expect(result[1]).toHaveProperty("name", "shaman");
+  });
 
-    test('findById()', async () => {
-        let result = await classes.findById(1);
-        expect(result).toBeDefined();
-        expect(result.class_id).toBe(1);
-        expect(result.name).toBe('warrior');
+  test("findById()", async () => {
+    let result = await classes.findById(1);
+    expect(result).toBeDefined();
+    expect(result.class_id).toBe(1);
+    expect(result.name).toBe("warrior");
 
-        result = await classes.findById(20);
-        expect(result).not.toBeDefined();
-    })
+    result = await classes.findById(20);
+    expect(result).not.toBeDefined();
+  });
 
-    test('insert()', async () => {
-        let result = await classes.insert({ name: 'mage' });
-        expect(result).toBeDefined();
-        expect(result.class_id).toBe(3);
-        expect(result.name).toBe('mage');
+  test("insert()", async () => {
+    let result = await classes.insert({ name: "mage" });
+    expect(result).toBeDefined();
+    expect(result.class_id).toBe(3);
+    expect(result.name).toBe("mage");
 
-        result = await classes.findAll();
-        expect(result).toHaveLength(3);
-        expect(result[2]).toMatchObject({ name: 'mage' });
-    });
+    result = await classes.findAll();
+    expect(result).toHaveLength(3);
+    expect(result[2]).toMatchObject({ name: "mage" });
+  });
 
-})
+});
+
+describe("HTTP tests", () => {
+
+  test("API is up", async () => {
+    let res = await request(server).get("/");
+    expect(res).toBeDefined();
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ api: "up" });
+  });
+
+  test('GET /api/classes', async () => {
+    let res = await request(server).get('/classes');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body).toHaveLength(2);
+});
+  
+});
