5fc4b530092321c6a7be6fe6e14c3d5e0671b22b
John Ruddock <john.jxr@pm.me>
2022-06-24 11:14:37 -0400

    add 2 more endpoints

--- api/wow-classes/router.js
+++ api/wow-classes/router.js
@@ -4,4 +4,12 @@ router.get('/', (req, res) => {
     res.send('Hello from wow-classes route');
 });
 
+router.post('/', (req, res) => {
+    res.send('Hello from wow-classes post route');
+});
+
+router.get('/:id', (req, res) => {
+    res.send('Hello from wow-classes get by ID route');
+});
+
 module.exports = router;
